acf8c1a2c3b5aeda8abfb6fb2737bfc6fceb3082
Ajaya Shrestha <ajaya92@gmail.com>
2021-07-29 21:59:53 -0500

    Working Credit Card check - need to work on Button Event listener

---
+++ js/script.js
@@ -0,0 +1,126 @@
+var creditCardNum = document.getElementById("creditCardNum").value;
+var creditCardIssue = document.getElementById("resultBox");
+//const checkButton = document.getElementById("checkButton");
+const validateButton = document.querySelector("#checkButton");
+
+/*
+const validateCreditCard = (num)=>{
+    var arrayList = String(num).split("").map((num)=>{
+        return Number(num);
+    })
+    
+        if((arrayList[0]== 3 && arrayList[1]==4 && arrayList.length == 15) || (arrayList[0]== 3 && arrayList[1]==7 && arrayList.length == 15) ){
+
+            var img = document.createElement('img');
+            img.src = "/images/ac.webp"
+            img.width = "400";
+            img.height = "200";
+            document.getElementById('resultDiv').appendChild(img);
+
+
+           
+        }
+        else  if((arrayList[0]== 5 && arrayList[1]==4 && arrayList.length == 16) ){
+
+            var img = document.createElement('img');
+            img.src = "/images/DC.jpg"
+            img.width = "400";
+            img.height = "200";
+            document.getElementById('resultDiv').appendChild(img);
+
+
+           
+        }
+        else  if((arrayList[0]== 6 && arrayList[1]== 0 && arrayList[2]== 1 && arrayList[3]== 1) ||
+         (arrayList[0]== 6 && arrayList[1]== 2 && arrayList[2]== 2 && (arrayList[3]>= 1 && arrayList[3]<=9))
+         ||
+         (arrayList[0]== 6 && arrayList[1]== 4 && arrayList[2]== 4 ) || (arrayList[0]== 6 && arrayList[1]== 4 && arrayList[2]== 5 ) || (arrayList[0]== 6 && arrayList[1]== 4 && arrayList[2]== 6 ) ||
+         (arrayList[0]== 6 && arrayList[1]== 4 && arrayList[2]== 7 ) || (arrayList[0]== 6 && arrayList[1]== 4 && arrayList[2]== 8 ) || (arrayList[0]== 6 && arrayList[1]== 4 && arrayList[2]== 9 ) || 
+         (arrayList[0]== 6 && arrayList[1]== 5 )
+        && (arrayList.length>=16 && arrayList.length <=19)
+        ){
+
+            var img = document.createElement('img');
+            img.src = "/images/discover.webp"
+            img.width = "400";
+            img.height = "200";
+            document.getElementById('resultDiv').appendChild(img);
+
+
+           
+        }
+
+        else  if((arrayList[0]== 5 && arrayList[1]>=1 && arrayList[1]<= 5)        
+       ){
+
+           var img = document.createElement('img');
+           img.src = "/images/me.png"
+           img.width = "400";
+           img.height = "200";
+           document.getElementById('resultDiv').appendChild(img);
+
+
+          
+       }
+       else  if((arrayList[0]== 4 && arrayList.length >=13 && arrayList.length <=19)        
+       ){
+
+           var img = document.createElement('img');
+           img.src = "/images/visa.webp"
+           img.width = "400";
+           img.height = "200";
+           document.getElementById('resultDiv').appendChild(img);
+
+
+          
+       }
+        
+        else{
+            creditCardIssue.innerHTML = " "
+            
+
+        }
+    
+
+}
+*/
+var validateCardNum = (num) =>{
+    var arrayList = String(num).split("").map((num)=>{
+        return Number(num);
+    })
+    arrayList.pop();
+    arrayList.reverse();
+    for(let i =0; i<arrayList.length; i++){
+        if (arrayList[i]%2 !=0){
+            arrayList[i] *= 2;
+            if(arrayList[i] >9){
+
+                arrayList[i] -= 9;
+                
+            }
+        }
+
+    }
+   var sum = arrayList.reduce((a,b)=>a+b,0);
+   if(sum > 0){
+    var finalMod = sum%10;
+    if(finalMod == 0 ){
+        document.getElementById("resultBox").innerHTML = "Valid Card";
+    }
+    else{
+     document.getElementById("resultBox").innerHTML = "Invalid Card";
+ 
+        
+    }
+
+   }
+
+
+
+}
+
+
+
+//checkButton.addEventListener('click', validateCreditCard(creditCardNum));
+validateButton = window.addEventListener('click', validateCardNum(creditCardNum));
+
